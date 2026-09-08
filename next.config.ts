a76a03b152fd4b409996393ff0958d967b72fcbc
William Alfred <william@fredonly.com>
2026-09-08 04:45:19 +0100

    fix: remove global content-type override

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,5 @@
-﻿import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/((?!api(?:/|$)).*)",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "text/html; charset=utf-8",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
